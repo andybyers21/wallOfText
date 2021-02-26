689e733b50c11ca1d250a75b4728f523ad9798ea
andybyers21 <andy.byers21@gmail.com>
2021-02-26 13:40:58 +0000

    add bullet points, line breaks etc

--- process.py
+++ process.py
@@ -2,18 +2,20 @@ def text_process(text):
     """ 
     Process user input text.
     """
-    # Identify line breaks and insert <hr> tag 
     # NOTE: the last line will also have an orphan <p> tag
     # NOTE: this assumes proper use of full stops in the input text
-    ### text_a = text.removeprefix('"').removesuffix('"')
-    text_b = "<p>" + text
-    new_text = text_b.replace('.', '.</p>\n<p>')
-
-
-    # Add bullet point to first line
     
-    # Split lines at full stop and add bullet point
+    # Identify line breaks and insert <hr> tag 
+    text = text.replace("\n", "<hr>" )
 
-    return new_text
+    # Add bullet point and <p> tag to first line
+    text = "<p>- " + text
+
+    # Split lines at full stop and add bullet point
+    text = text.replace('.', '.</p>\n<p>- ')
+
+    # Remove the orphan bullet point and <p> tag
+
+    return text
 
 
