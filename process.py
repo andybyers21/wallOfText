8f49efbd5e697f97ebaf9098758f193ababe496a
andybyers21 <andy.byers21@gmail.com>
2021-02-28 12:01:50 +0000

    implement regex solution to html output

--- process.py
+++ process.py
@@ -7,16 +7,22 @@ def text_process(text):
     text, takes user sumitted plain text and formats it as per the WoT
     specification.
     """
-#    text = text.replace("\r\n\r\n", "<br><hr><br>")
-    text_list = text.split(".")
+    text = re.sub("\r\n\r\n", "<hr>", text)
+    text_list = re.findall('.*?[.!\?]+', text)
+    # TODO: also need to check for speach in text ""
     new_list = []
 
     for item in text_list:
-        new_string = "<li>" + item + ".</li>"
+        new_string = item.lstrip()
+        new_string = new_string.capitalize()
+        new_string = "<li>" + new_string + "</li>"
+        new_string = re.sub('<li><hr>', '<hr><br><li>', new_string)
         new_list.append(new_string)
+    
+    str1 = ""  
+    
+    for ele in new_list:  
+        str1 += ele   
+    
+    return str1 
 
-    new_list = new_list[:-1]
-    new_string = re.sub('<li>.</li>', '', new_string)
-    new_string = new_string.join(new_list)
-
-    return new_string
